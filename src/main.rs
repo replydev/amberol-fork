32648c1479930f90f0a5f2c395796171b153c7ab
Emmanuele Bassi <ebassi@gnome.org>
2022-05-02 14:51:41 +0100

    Remove unnecessary clone

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
             .expect("Unable to find amberol.gresource"),
         Ok(_) => match env::current_exe() {
             Ok(path) => {
-                let mut resource_path = path.clone();
+                let mut resource_path = path;
                 resource_path.pop();
                 resource_path.push("amberol.gresource");
                 gio::Resource::load(&resource_path)
